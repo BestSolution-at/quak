5a768c3339a669e28997433fd746713fe619be9a
Kerim Yeniduenya <kerim.yeniduenya@bestsolution.at>
2022-03-25 09:58:47 +0100

    allowRedeploy set "true" by default fixes #13

--- src/main/java/at/bestsolution/quak/QuakConfiguration.java
+++ src/main/java/at/bestsolution/quak/QuakConfiguration.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.util.List;
 
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
 @ConfigMapping( prefix = "quak" )
 public interface QuakConfiguration {
@@ -13,6 +14,8 @@ public interface QuakConfiguration {
 		public String name();
 		public Path storagePath();
 		public String baseUrl();
+		
+		@WithDefault("true")
 		public boolean allowRedeploy();
 	}
 }
