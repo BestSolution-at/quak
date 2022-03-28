d2ab10e0e32f62548fb80d44435910017a2d4d5d
Kerim Yeniduenya <kerim.yeniduenya@bestsolution.at>
2022-03-28 14:20:36 +0200

    quak test profile added refs #6

--- src/test/java/at/bestsolution/quak/QuakResourceTest.java
+++ src/test/java/at/bestsolution/quak/QuakResourceTest.java
@@ -4,19 +4,22 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
+import javax.ws.rs.core.Response.Status;
+
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
 
 @QuarkusTest
+@TestProfile(QuakTestProfile.class)
 public class QuakResourceTest {
 
 	@Test
 	void testGet() {
-		given().when().get().then().statusCode( 200 );
-
+		given().when().get().then().statusCode( Status.OK.getStatusCode() );
 	}
 
 	@Test
 	void testUpload() {
-		given().put().then().statusCode( 200 );
+		given().request().body( "dummy file" ).put( "/at/bestsolution/blueprint/dummy_file.txt" ).then().statusCode( Status.OK.getStatusCode() );
 	}
 }
