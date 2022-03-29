242d7776864e30e8e0d6012b330943db9ed88309
Kerim Yeniduenya <kerim.yeniduenya@bestsolution.at>
2022-03-29 15:14:02 +0200

    test cases added refs #6

--- src/test/java/at/bestsolution/quak/QuakResourceTest.java
+++ src/test/java/at/bestsolution/quak/QuakResourceTest.java
@@ -1,25 +1,38 @@
 package at.bestsolution.quak;
 
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 
 import javax.ws.rs.core.Response.Status;
+
+import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 
 @QuarkusTest
-@TestProfile(QuakTestProfile.class)
+@TestProfile( QuakTestProfile.class )
 public class QuakResourceTest {
 
 	@Test
 	void testGet() {
-		given().when().get().then().statusCode( Status.OK.getStatusCode() );
+		given().when().get( "/at/bestsolution/blueprint/" ).then().statusCode( Status.OK.getStatusCode() );
+	}
+
+	@Test
+	void testGetWrongPath() {
+		given().when().get( "/at/wrong/path/" ).then().statusCode( Status.NOT_FOUND.getStatusCode() );
 	}
 
 	@Test
 	void testUpload() {
-		given().request().body( "dummy file" ).put( "/at/bestsolution/blueprint/dummy_file.txt" ).then().statusCode( Status.OK.getStatusCode() );
+		given().request().body( "dummy file" ).put( "/at/bestsolution/blueprint/dummy_file.foo" ).then().statusCode( Status.OK.getStatusCode() );
+
+		// Re-deploy
+		given().request().body( "dummy file" ).put( "/at/bestsolution/blueprint/dummy_file.foo" ).then().statusCode( Status.OK.getStatusCode() );
+	}
+
+	@Test
+	void testUploadWrongPath() {
+		given().request().body( "dummy file" ).put( "/at/wrong/path/dummy_file.foo" ).then().statusCode( Status.NOT_FOUND.getStatusCode() );
 	}
 }
