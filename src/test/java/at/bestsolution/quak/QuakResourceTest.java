d57ce027f57488ab1a1ddc97441106f0e29d0d74
Kerim Yeniduenya <kerim.yeniduenya@bestsolution.at>
2022-03-29 15:24:50 +0200

    test order updated refs #6

--- src/test/java/at/bestsolution/quak/QuakResourceTest.java
+++ src/test/java/at/bestsolution/quak/QuakResourceTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.given;
 
 import javax.ws.rs.core.Response.Status;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -14,16 +15,19 @@ import io.quarkus.test.junit.TestProfile;
 public class QuakResourceTest {
 
 	@Test
+	@Order( 4 )
 	void testGet() {
 		given().when().get( "/at/bestsolution/blueprint/" ).then().statusCode( Status.OK.getStatusCode() );
 	}
 
 	@Test
+	@Order( 3 )
 	void testGetWrongPath() {
 		given().when().get( "/at/wrong/path/" ).then().statusCode( Status.NOT_FOUND.getStatusCode() );
 	}
 
 	@Test
+	@Order( 2 )
 	void testUpload() {
 		given().request().body( "dummy file" ).put( "/at/bestsolution/blueprint/dummy_file.foo" ).then().statusCode( Status.OK.getStatusCode() );
 
@@ -32,6 +36,7 @@ public class QuakResourceTest {
 	}
 
 	@Test
+	@Order( 1 )
 	void testUploadWrongPath() {
 		given().request().body( "dummy file" ).put( "/at/wrong/path/dummy_file.foo" ).then().statusCode( Status.NOT_FOUND.getStatusCode() );
 	}
