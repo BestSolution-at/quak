b7c08d0090b4f144995e3de41eab348ba4f59d53
Kerim Yeniduenya <kerim.yeniduenya@bestsolution.at>
2022-04-27 13:36:07 +0200

    test for wrong username token added refs #15

--- src/test/java/at/bestsolution/quak/QuakSecurityInterceptorTest.java
+++ src/test/java/at/bestsolution/quak/QuakSecurityInterceptorTest.java
@@ -58,8 +58,18 @@ class QuakSecurityInterceptorTest {
 										+ "r6Sp9Awm8-3O--TABGzI-ss8iXKadagUoO_ZS6VAbS2JikhU1TBn0yt49ti3aqD"
 										+ "Fyyr-7_cCeQT2NfX42VHnNIXAHZUsWKVNyWoyzqxpq_I0zH1c19KSgiQ3WRl9s6"
 										+ "Z-rnHXtUNNwLOD2YMW0bs5XO2k_hlA78KLMbfqJRe9xCqMFN6NRJ_w1JA7uwQFl"
-										+ "weM_8gH40BSg1Ui3Kwq5exxit-KNAEeYw\n";
+										+ "weM_8gH40BSg1Ui3Kwq5exxit-KNAEeYw";
 	private static final String WRONG_TOKEN = "WRONG_TOKEN";
+	private static final String TOKEN_WITH_WRONGUSERNAME = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3d3dy"
+										+ "5iZXN0c29sdXRpb24uYXQvcXVhay90ZXN0IiwidXBuIjoidXNlcldyb25nIiwiZ"
+										+ "3JvdXBzIjpbIlVzZXIiLCJBZG1pbiJdLCJleHAiOjE2ODI1OTQ1OTYsImlhdCI6"
+										+ "MTY1MTA1ODU5NiwianRpIjoiMzA1MWFiZGMtYWViZC00MjlmLTgyZmMtNGFmMTl"
+										+ "jMTFkMmZlIn0.dAJxTa10a8nPbu2ueQ67rRd4uPZarS470kp0lpF9cYTbGTjwTG"
+										+ "dW4mTvnqFByZdc6WtUtbKg2qluFDacFALYkOvbCaKJ9rPwvnMqgSAj3Iv-MfY3U"
+										+ "lXT-9awuYe_p5bjHR_sEJgAYwlw1G6MHvX6Ib1ypXHRvqQWNFyZ3a9V_L6xqp0q"
+										+ "4IiKUpvpOOe0140Z7qJOvh-s_r1Kwk9dhSiTE3M61nbNuqFQrv1qFoeovGkLTcF"
+										+ "MOXmexy0rebvRtd8Z66R_fJTQObRXtyiG5f6F5yNF6SwLry6to2wcBsEFDEZjoi"
+										+ "pkNnXmB9EwlfnWBfTjz7dJFPo6l9APipUl8tYHYw";
 	
 	
 	/**
@@ -87,15 +97,17 @@ class QuakSecurityInterceptorTest {
 	}
 	
 	/**
-	 * Asserts authorization with tokens are done correctly.
+	 * Asserts authentication with tokens are done correctly.
 	 */
 	@Test
 	@Order( 2 )
-	void testTokenAuthorization() {
+	void testTokenAuthentication() {
 		given().auth().oauth2( TOKEN ).request().body( QuakResourceTest.DUMMY_FILE_CONTENT ).put( QuakResourceTest.DUMMY_FILE_FOO ).then().statusCode( Status.OK.getStatusCode() );
 		given().auth().oauth2( WRONG_TOKEN ).request().body( QuakResourceTest.DUMMY_FILE_CONTENT ).put( QuakResourceTest.DUMMY_FILE_FOO ).then().statusCode( Status.UNAUTHORIZED.getStatusCode() );
+		given().auth().oauth2( TOKEN_WITH_WRONGUSERNAME ).request().body( QuakResourceTest.DUMMY_FILE_CONTENT ).put( QuakResourceTest.DUMMY_FILE_FOO ).then().statusCode( Status.UNAUTHORIZED.getStatusCode() );
 		
 		given().auth().oauth2( TOKEN ).get( QuakTestProfile.BASE_URL.concat( "/" ) ).then().statusCode( Status.OK.getStatusCode() );
 		given().auth().oauth2( WRONG_TOKEN ).get( QuakTestProfile.BASE_URL.concat( "/" ) ).then().statusCode( Status.UNAUTHORIZED.getStatusCode() );
+		given().auth().oauth2( TOKEN_WITH_WRONGUSERNAME ).get( QuakTestProfile.BASE_URL.concat( "/" ) ).then().statusCode( Status.UNAUTHORIZED.getStatusCode() );
 	}
 }
