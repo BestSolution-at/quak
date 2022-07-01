d9562a6a3d12bc03c1584c725aa9e955b7b9e7be
Kerim Yeniduenya <kerim.yeniduenya@bestsolution.at>
2022-07-01 11:57:37 +0200

    added JWT, OAuth2 and OIDC configuration mappings refs #29

--- src/main/java/at/bestsolution/quak/QuakConfiguration.java
+++ src/main/java/at/bestsolution/quak/QuakConfiguration.java
@@ -27,6 +27,7 @@ package at.bestsolution.quak;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
@@ -44,6 +45,24 @@ public interface QuakConfiguration {
 	 * 		HTTP configuration of quak.
 	 */
 	public HTTP http();
+	
+	/**
+	 * @return
+	 * 		JWT configuration of quak.
+	 */
+	public JWT jwt();
+	
+	/**
+	 * @return
+	 * 		OAuth2 configuration of quak.
+	 */
+	public OAuth2 oauth2();
+	
+	/**
+	 * @return
+	 * 		OpenID Connect configuration of quak.
+	 */
+	public OIDC oidc();
 	
 	/**
 	 * @return
@@ -81,10 +100,104 @@ public interface QuakConfiguration {
 		
 		/**
 		 * @return 
+		 * 		the TCP host quak will be listening on.
+		 */
+		@WithDefault("0.0.0.0")
+		public String host();
+		
+		/**
+		 * @return 
 		 * 		the max size of uploaded artifacts.
 		 */
 		@WithDefault("10240K")
 		public String maxBodySize();
+	}
+	
+	/**
+	 * Represents a quak JWT configuration.
+	 */
+	public interface JWT {
+		
+		/**
+		 * @return 
+		 * 		location of JWT token issuer public key.
+		 */
+		public Optional<String> issuerPublicKey();
+		
+		/**
+		 * @return 
+		 * 		name of JWT token issuer.
+		 */
+		public Optional<String> issuerName();
+	}
+	
+	/**
+	 * Represents a quak OAuth2 configuration.
+	 */
+	public interface OAuth2 {
+		
+		/**
+		 * @return 
+		 * 		if the OAuth2 extension is enabled.
+		 */
+		@WithDefault("false")
+		public boolean enabled();
+		
+		/**
+		 * @return 
+		 * 		OAuth2 client id used to validate the token. Mandatory if the OAuth2 is enabled.
+		 */
+		public Optional<String> clientId();
+		
+		/**
+		 * @return 
+		 * 		OAuth2 client secret used to validate the token. Mandatory if the OAuth2 is enabled.
+		 */
+		public Optional<String> clientSecret();
+		
+		/**
+		 * @return 
+		 * 		OAuth2 introspection endpoint URL used to validate the token. Mandatory if the extension is enabled.
+		 */
+		public Optional<String> introspectionUrl();
+	}
+	
+	/**
+	 * Represents a quak OpenID Connect configuration.
+	 */
+	public interface OIDC {
+		
+		/**
+		 * @return 
+		 * 		if the OIDC extension is enabled.
+		 */
+		@WithDefault("false")
+		public boolean enabled();
+		
+		/**
+		 * @return 
+		 * 		OIDC client id used to validate the token. Mandatory if the OIDC is enabled.
+		 */
+		public Optional<String> clientId();
+		
+		/**
+		 * @return 
+		 * 		OIDC client secret used to validate the token. Mandatory if the OIDC is enabled.
+		 */
+		public Optional<String> sharedSecret();
+		
+		/**
+		 * @return 
+		 * 		base URL of OIDC server, for example, https://host:port/auth.
+		 */
+		public Optional<String> authServerUrl();
+		
+		/**
+		 * @return 
+		 * 		if this property is set to 'true' then an OIDC UserInfo endpoint will be called. Must be true for quak OIDC authentication.
+		 */
+		@WithDefault("true")
+		public boolean userInfoRequired();
 	}
 	
 	/**
--- src/main/java/at/bestsolution/quak/QuakConfigurationRelocateInterceptor.java
+++ src/main/java/at/bestsolution/quak/QuakConfigurationRelocateInterceptor.java
@@ -31,27 +31,48 @@ import java.util.function.UnaryOperator;
 import io.smallrye.config.RelocateConfigSourceInterceptor;
 
 /**
- * Interceptor class for relocating Quarkus configuration names to quak configuration names.
+ * Interceptor class for relocating Quarkus configuration names to quak
+ * configuration names.
  * 
  * @author kerim.yeniduenya@bestsolution.at
  */
 public class QuakConfigurationRelocateInterceptor extends RelocateConfigSourceInterceptor {
-	
-    /**
-     * Transformation function to relocate required Quarkus configuration names to 
-     * quak configuration names.  
-     */
-    private static final UnaryOperator<String> RELOCATE = name -> {
-        if (name.startsWith( "quarkus.http.port" ) ) {
-            return name.replaceFirst( "quarkus\\.http\\.port", "quak.http.port" );
-        }
-        if (name.startsWith( "quarkus.http.max-body-size" ) ) {
-            return name.replaceFirst( "quarkus\\.http\\.max-body-size", "quak.http.max-body-size" );
-        }
-        return name;
-    };
 
-    public QuakConfigurationRelocateInterceptor() {
-        super( RELOCATE );
-    }
+	/**
+	 * Transformation function to relocate required Quarkus configuration names
+	 * to quak configuration names.
+	 */
+	private static final UnaryOperator<String> RELOCATE = name -> {
+
+		if ( name.startsWith( "quarkus.http.limits." ) ) {
+			return name.replaceFirst( "quarkus\\.http\\.limits\\.", "quak.http." );
+		} 
+		else if ( name.startsWith( "quarkus.http." ) ) {
+			return name.replaceFirst( "quarkus\\.http\\.", "quak.http." );
+		} 
+		else if ( name.startsWith( "quarkus.oauth2." ) ) {
+			return name.replaceFirst( "quarkus\\.oauth2\\.", "quak.oauth2." );
+		} 
+		else if ( name.startsWith( "quarkus.oidc.authentication." ) ) {
+			return name.replaceFirst( "quarkus\\.oidc\\.authentication.", "quak.oidc." );
+		}
+		else if ( name.startsWith( "quarkus.oidc.credentials.secret" ) ) {
+			return name.replaceFirst( "quarkus\\.oidc\\.credentials\\.secret", "quak.oidc.shared-secret" );
+		} 
+		else if ( name.startsWith( "quarkus.oidc." ) ) {
+			return name.replaceFirst( "quarkus\\.oidc\\.", "quak.oidc." );
+		}
+		else if ( name.startsWith( "mp.jwt.verify.publickey.location" ) ) {
+			return name.replaceFirst( "mp\\.jwt\\.verify\\.publickey\\.location", "quak.jwt.issuer-public-key" );
+		} 
+		else if ( name.startsWith( "mp.jwt.verify.issuer" ) ) {
+			return name.replaceFirst( "mp\\.jwt\\.verify\\.issuer", "quak.jwt.issuer-name" );
+		}
+
+		return name;
+	};
+
+	public QuakConfigurationRelocateInterceptor() {
+		super( RELOCATE );
+	}
 }
