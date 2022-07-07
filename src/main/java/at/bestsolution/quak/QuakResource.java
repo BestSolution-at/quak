be1883af46ef54cb9ac6fe1526485c5fe91c8a64
Kerim Yeniduenya <kerim.yeniduenya@bestsolution.at>
2022-07-07 08:49:46 +0200

    removed unused collectors import refs #40

--- src/main/java/at/bestsolution/quak/QuakResource.java
+++ src/main/java/at/bestsolution/quak/QuakResource.java
@@ -39,7 +39,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.enterprise.event.Observes;
